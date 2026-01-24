221bb0087bfb50a82143501c6eb550d90700239e
Thiago Gomes <thiago.g.sousa100@gmail.com>
2026-01-24 13:18:17 -0300

    add: Adicionar receitas.route.js

--- API/server.js
+++ API/server.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import receitasRoutes from './src/routes/receitas.route'
 
 const app = express()
 const PORT = 3001
+
+app.use(express.json())
+
+app.use('/api/receitas', receitasRoutes)
 
 app.listen(PORT, () => {
     console.log(`Servidor Rodando na Porta ${PORT}`)
