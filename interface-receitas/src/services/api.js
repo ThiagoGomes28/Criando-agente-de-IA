9e87f95a9d26601a86e64657f2c60ec0bdafb275
Thiago Gomes <thiago.g.sousa100@gmail.com>
2026-01-28 16:03:17 -0300

    Add: Adicionar api.js

---
+++ interface-receitas/src/services/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const API_URL = import.meta.env.API_URL
+
+export const api = async (pergunta) => {
+    try {
+
+        const response = await axios.post(`${API_URL}receitas/perguntar`, {
+            pergunta
+        })
+
+        return response.data.resposta
+
+    } catch(err) {
+        console.error("Error ao buscar resposta no servidor ", err)
+        throw error
+    }
+}
